6fc939ebab48abc7fb6128aa8a5f85fe6d140199
Yegan <yeganj@gmail.com>
2018-07-23 16:17:53 +0200

    intial commit

--- greet_xola.js
+++ greet_xola.js
@@ -1,6 +1,22 @@
+"use strict"
+const figlet = require("figlet")
 const chalk = require('chalk');
 //import the greet module that is in the current folder
 const greet = require('./greet');
 
 const styledMessage = chalk.bgGreen.black(greet('Xola'));
 console.log(styledMessage)
+
+
+figlet.text('Xola!', {
+  font: 'Ghost',
+  horizontalLayout: 'default',
+  verticalLayout: 'default'
+}, function(err, data) {
+  if (err) {
+    console.log('Something went wrong...');
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
